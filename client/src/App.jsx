22ce692e60dba5dd5f5452a9e7dfd63c90c7c4ca
dev-nischay <nischay.mehn@gmail.com>
2025-10-31 17:51:05 +0530

    added login route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { MainLayout } from "./layouts/MainLayout";
-import { Home } from "./components/home/Home";
+import { Home } from "./components/home/SignUp.jsx";
+import { Login } from "./components/home/SignIn.jsx";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
