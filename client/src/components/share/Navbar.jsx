77347bc771bec0d9fe630658de3edad5a89cbfb8
dev-nischay <nischay.mehn@gmail.com>
2025-11-03 19:12:58 +0530

    added scalable navbar

--- client/src/components/share/Navbar.jsx
+++ client/src/components/share/Navbar.jsx
@@ -2,12 +2,12 @@ import { Menu } from "lucide-react";
 
 export const Navbar = () => {
   const links = [
-    { name: "Login", href: "" },
+    { name: "Login", href: "/login" },
     { name: "Social", href: "https://x.com/nischaytwt" },
     { name: "Github", href: "https://github.com/dev-nischay" },
   ];
   return (
-    <div className="text-neutral-300  h-full w-full  text-sm max-w-sm   mx-auto flex items-center border border-neutral-900  justify-between rounded-full mt-1  px-4 p-2 md:max-w-2xl lg:max-w-3xl xl:max-w-4xl 2xl:max-w-[70%]  lg:text-lg 2xl:text-4xl   md:px-6  lg:p-3 lg:px-8 xl:  2xl:p-10  2xl:px-12">
+    <div className="text-neutral-300  h-full w-full  text-sm max-w-sm   mx-auto flex items-center border border-neutral-900  justify-between rounded-full mt-1  px-4 p-2 md:max-w-2xl lg:max-w-3xl xl:max-w-4xl 2xl:max-w-[60%]   xl:text-lg    md:px-6  lg:p-3 lg:px-8  2xl:p-5    2xl:px-10   ">
       <div>Learnspace</div>
       <div className="lg:hidden lg:pointer-events-none">
         <Menu />
