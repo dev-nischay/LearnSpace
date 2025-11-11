15fb33383c48078a2a6f80f2ae759f8ae01a8239
dev-nischay <nischay.mehn@gmail.com>
2025-11-11 12:55:15 +0530

    updated scalable schema to accept multiple documents with subdocuments for transactions

--- server/src/models/purchasedCourses.ts
+++ server/src/models/purchasedCourses.ts
@@ -1,23 +1,36 @@
 import mongoose, { Mongoose } from "mongoose";
 import { Types } from "mongoose";
-const Schema = mongoose.Schema;
 
+const Schema = mongoose.Schema;
 const purchaseSchema = new Schema({
-  courseId: {
-    required: true,
-    type: Types.ObjectId,
-    ref: "course",
-  },
   userId: {
-    required: true,
     type: Types.ObjectId,
     ref: "user",
-  },
-  creatorId: {
     required: true,
-    type: Types.ObjectId,
-    ref: "admin",
   },
+  purchasedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  currency: {
+    type: String,
+    required: true,
+    default: "INR",
+  },
+
+  items: [
+    {
+      courseId: {
+        type: Types.ObjectId,
+        ref: "course",
+        required: true,
+      },
+    },
+  ],
 });
 
 export const Purchases = mongoose.model("purchase", purchaseSchema);
