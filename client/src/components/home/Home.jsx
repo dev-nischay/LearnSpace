b868ebdd4deed4a7a3dec0de77e0597a1a9f33b3
dev-nischay <nischay.mehn@gmail.com>
2025-11-12 21:43:07 +0530

    updated course page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
-import CourseCard from "../share/Course";
+import CourseCard from "./Course.jsx";
 import { useFetch } from "../../hooks/fetch";
 import { useAuthStore } from "../../store/authStore";
+import { images } from "../../utils/fake-data";
+import { usePurchaseStore } from "../../store/purchaseStore";
+import { useCartStore } from "../../store/cartStore";
 export const Home = () => {
-  const [courses, setCourses] = useState([]);
-  console.log(courses);
+  const courses = usePurchaseStore((state) => state.courses);
+  const setCourses = usePurchaseStore((state) => state.setCourses);
+  const clearCart = useCartStore((state) => state.clearCart);
   const data = useAuthStore((state) => state.creds);
-  console.log(data);
   const { error, request, loading } = useFetch("courses", {
     method: "GET",
     requiresAuth: true,
@@ -15,20 +18,33 @@ export const Home = () => {
   useEffect(() => {
     const get = async () => {
       try {
-        const data = await request();
-        setCourses(data.courses);
+        const res = await request();
+        setCourses(res.courses);
       } catch (error) {
         alert(error.message);
+        console.log(error);
       }
     };
     get();
   }, []);
 
   return (
-    <div className="min-h-screen w-full lg:p-8">
-      <h1 className="text-white m-2 text-xl font-semibold md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
-        Welcome {data.username}
+    <div className="min-h-screen w-full lg:p-8 xl:max-w-[90rem] mx-auto">
+      {loading && (
+        <div className="fixed h-screen backdrop-blur-md text-white inset-0 flex justify-center items-center">
+          Loading...
+        </div>
+      )}
+      <h1 className="text-white m-2 mb-10 text-xl font-semibold md:text-2xl lg:text-3xl ">
+        Welcome {data.username}{" "}
+        <button
+          onClick={() => clearCart()}
+          className="bg-red-500 px-2  rounded-lg text-black"
+        >
+          clearCart test
+        </button>
       </h1>
+
       <div className="grid grid-cols-1 gap-12 w-full  mx-auto  md:grid-cols-2 lg:grid-cols-3">
         {courses.map((e, i) => (
           <CourseCard
@@ -37,6 +53,7 @@ export const Home = () => {
             description={e.description}
             price={e.price}
             courseId={e._id}
+            image={images[i]}
           />
         ))}
       </div>
