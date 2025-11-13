8b234152547831edcb095fb65c2809a5c0fd7d19
dev-nischay <nischay.mehn@gmail.com>
2025-11-13 19:21:33 +0530

    added fake images will be replaced later

---
+++ client/src/utils/fake-data.js
@@ -0,0 +1,8 @@
+export const images = [
+  "https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=800",
+  "https://images.unsplash.com/photo-1523240795612-9a054b0db644?auto=format&fit=crop&w=800&q=80",
+  "https://images.unsplash.com/photo-1519389950473-47ba0277781c?auto=format&fit=crop&w=800&q=80",
+  "https://www.corpnce.com/wp-content/uploads/2020/05/machine-learning.jpg",
+  "https://media.licdn.com/dms/image/v2/D5612AQEmkUuLiO1VRA/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1690697395718?e=2147483647&v=beta&t=vbZc8sUEFw1WiRGBAni4UN-F1YtUoD79_smZcEmZJko",
+  "https://frontendmastery.com/og.png?v=2",
+];
