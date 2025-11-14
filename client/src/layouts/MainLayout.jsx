679c51a9e0dc5ad517cd47cf1058124476616922
dev-nischay <nischay.mehn@gmail.com>
2025-11-14 19:55:53 +0530

    added Sidebar to layout

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -8,9 +8,9 @@ export const MainLayout = () => {
       <div className="absolute bottom-0 left-0 w-[70vw] h-[70vh] bg-[radial-gradient(circle_at_bottom_left,rgba(180,200,220,0.25),transparent_80%)] blur-3xl animate-glow2 pointer-events-none "></div>
 
       <div className="absolute top-1/3 left-1/3 w-[50vw] h-[50vh] bg-[radial-gradient(circle,rgba(255,255,255,0.1),transparent_70%)] blur-2xl animate-glow3 pointer-events-none "></div>
-      {/* <Sidebar /> */}
+      <Sidebar />
       <Navbar />
-      <div className="p-4 mt-10 mx-auto lg:max-w-5xl xl:max-w-[90rem]">
+      <div className="p-4 my-10 mx-auto lg:max-w-5xl xl:max-w-[90rem]">
         <Outlet />
       </div>
     </div>
