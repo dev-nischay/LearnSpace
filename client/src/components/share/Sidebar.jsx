cbc22829844d8ed1b298a18403024cc0a9db78cc
dev-nischay <nischay.mehn@gmail.com>
2025-11-14 19:54:13 +0530

    updated  working sidebar on all screen sizes

--- client/src/components/share/Sidebar.jsx
+++ client/src/components/share/Sidebar.jsx
@@ -1,82 +1,72 @@
-import { Menu, SidebarOpen } from "lucide-react";
-import { HomeIcon } from "lucide-react";
+import { SidebarOpen } from "lucide-react";
 import { Github } from "lucide-react";
+import { Bookmark } from "lucide-react";
 import { CreditCard } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { HomeIcon } from "lucide-react";
 
 export const Sidebar = () => {
   const [open, setOpen] = useState(false);
+  const [slide, setSlide] = useState(false);
   const icons = [
     { name: "Home", svg: <HomeIcon /> },
     { name: "Github", svg: <Github /> },
-    { name: "CreditCard", svg: <CreditCard /> },
+    { name: "Purchases", svg: <CreditCard /> },
+    { name: "Bookmark", svg: <Bookmark /> },
   ];
+
+  useEffect(() => {
+    const timer = setTimeout(() => setSlide(true), 200);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div
-      className={`bg-[#0a0b10] fixed left-0 inset-y-0  border-r-[1px] transition-all duration-300 z-50 transform border-secondary     text-white ${
-        open ? "w-52" : "w-18"
-      }`}
+      className={`bg-[#0a0b10] fixed inset-x-0 bottom-0   2xl:left-0 2xl:inset-y-0  border-t-[1px] 2xl:border-r-[1px] transition-all duration-300 z-50 transform border-secondary sm:px-3 md:px-6 lg:px-10 xl:px-16   2xl:px-0  text-white ${
+        open ? "2xl:w-52 " : "2xl:w-18"
+      } ${slide ? "translate-y-0" : "translate-y-20"}`}
     >
       <div
-        className={`text-white border-b-[1px] border-secondary flex-col flex gap-6 p-2  h-18 mb-5     `}
+        className={`text-white 2xl:border-b-[1px]     2xl:border-secondary  2xl:flex-col flex gap-6 p-2  h-18   `}
       >
-        <div
-          onClick={() => setOpen((prev) => !prev)}
-          className={`rounded flex items-center gap-2 pl-2  mt-5 transition-all  ${
-            open ? "hover:bg-secondary/20" : "bg-none"
-          }`}
-        >
-          <div className="flex gap-2  items-center">
-            <div
-              className={` ${
-                open ? "bg-none" : "hover:bg-secondary/20"
-              } pl-2  rounded-md`}
+        {/* Toggle Sidebar */}
+        <div className="2xl:flex gap-2  items-center  hidden hover:text-blue-500 rounded-md">
+          <div
+            onClick={() => setOpen((prev) => !prev)}
+            className=" 2xl:hover:bg-secondary/20 p-3 rounded-md"
+          >
+            <SidebarOpen />
+          </div>
+          {
+            <h1
+              className={`text-xl items-center   transition-all duration-200 ${
+                open ? "2xl:flex" : "2xl:hidden"
+              }`}
             >
-              <SidebarOpen />
-            </div>
-            {
-              <h1
-                className={`text-xl items-center   transition-all duration-200 ${
-                  open
-                    ? "opacity-100 pointer-events-auto "
-                    : "opacity-0 pointer-events-none "
+              Menu
+            </h1>
+          }
+        </div>
+
+        {/* Icons sections  */}
+        <div className="2xl:flex-col 2xl:gap-10 flex items-center  w-full  mt-3  justify-between 2xl:items-start ">
+          {icons.map((e, i) => (
+            <div
+              key={i}
+              className={`flex gap-1  items-center hover:text-blue-500     rounded-md transition-all `}
+            >
+              <div className={`rounded-md p-3  2xl:hover:bg-secondary/20`}>
+                {e.svg}
+              </div>
+              <div
+                className={`hidden  lg:flex ${
+                  open ? "2xl:flex" : "2xl:hidden"
                 }`}
               >
-                Menu
-              </h1>
-            }
-          </div>
-        </div>
-        <div>
-          <div className="flex-col gap-5 flex   ">
-            {icons.map((e, i) => (
-              <div key={i} className="p-2 rounded-md transition-all ">
-                <div
-                  className={` flex  ${
-                    open ? "hover:bg-secondary/20" : "bg-none"
-                  }  items-center gap-2 rounded-md`}
-                >
-                  <div
-                    className={`${
-                      open ? "bg-none" : "hover:bg-secondary/20"
-                    }  p-2 rounded-md`}
-                  >
-                    {e.svg}
-                  </div>
-                  <div
-                    className={` transition-all duration-200 ${
-                      open
-                        ? "opacity-100 pointer-events-auto"
-                        : "opacity-0 pointer-events-none"
-                    }`}
-                  >
-                    {e.name}
-                  </div>
-                </div>
+                {e.name}
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
